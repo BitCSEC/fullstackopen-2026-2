62440c8c501d300216c4f0c33e64f2c8d14a921f
BitCSEC <193537793+BitCSEC@users.noreply.github.com.>
2026-08-31 16:32:07 -0400

    uibuild

--- part3/index.js
+++ part3/index.js
@@ -12,6 +12,8 @@ morgan.token('body', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('dist'))
+
 
 let persons = [
     {
