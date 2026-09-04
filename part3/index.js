042c8fd3537bdf3a778cebdc5b96137088c0abbd
BitCSEC <193537793+BitCSEC@users.noreply.github.com.>
2026-09-04 15:32:14 -0400

    Completa ejercicio 3.22

--- part3/index.js
+++ part3/index.js
@@ -11,110 +11,110 @@ const cors = require('cors')
 app.use(cors())
 
 morgan.token('body', (request) => {
-    return JSON.stringify(request.body)
+  return JSON.stringify(request.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
 
-app.get('/api/people', (_, response, next) => {
-    Person
-        .find({})
-        .then(result => {
-            response.json(result)
-        })
-        .catch(error => next(error))
+app.get('/api/people', (request, response, next) => {
+  Person
+    .find({})
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/info', (_, response, next) => {
-    Person
-        .countDocuments({})
-        .then(count => {
-            const first_p = `<p>Phonebook has info for ${count} people</p>`
-            const second_p = `<p>${Date().toString()}</p>`
-            response.status(200).send(first_p + second_p)
-        })
-        .catch(error => next(error))
+app.get('/info', (request, response, next) => {
+  Person
+    .countDocuments({})
+    .then(count => {
+      const first_p = `<p>Phonebook has info for ${count} people</p>`
+      const second_p = `<p>${Date().toString()}</p>`
+      response.status(200).send(first_p + second_p)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/people/:id', (request, response, next) => {
-    Person
-        .findById(request.params.id)
-        .then(result => {
-            if (result) {
-                response.json(result)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person
+    .findById(request.params.id)
+    .then(result => {
+      if (result) {
+        response.json(result)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/people/:id', (request, response, next) => {
-    Person
-        .findByIdAndDelete(request.params.id)
-        .then(_ => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/people', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
     })
+  }
 
-    person
-        .save()
-        .then(_ => {
-            response.status(201).json(person)
-        })
-        .catch(error => next(error))
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then(() => {
+      response.status(201).json(person)
+    })
+    .catch(error => next(error))
 
 })
 
 app.put('/api/people/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    Person
-        .findById(request.params.id)
-        .then(person => {
-            if (!person) {
-                return response.status(404).end()
-            }
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
 
-            person.name = body.name
-            person.number = body.number
+      person.name = body.name
+      person.number = body.number
 
-            return person.save().then((updatedPerson) => {
-                response.json(updatedPerson)
-            })
-        })
-        .catch(error => next(error))
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    response.status(500).end()
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  response.status(500).end()
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
